25d73ff876c3795b7aab663cb08a1071c7b059ef
Ridwan Ajibola <rajibola1997@gmail.com>
2020-06-12 22:47:48 -0700

    all pages implemented

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="One">
+        <Stack.Screen name="One" component={PageOne} />
+        <Stack.Screen name="Two" component={PageTwo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function PageOne({ navigation }) {
+  return (
+    <View style={[styles.container, { backgroundColor: "limegreen" }]}>
+      <Button
+        title="go to Page two"
+        onPress={() => navigation.navigate("Two")}
+      />
     </View>
   );
 }
 
+function PageTwo({ navigation }) {
+  return (
+    <View style={[styles.container, { backgroundColor: "lightblue" }]}>
+      <Button title="go to Page One" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
